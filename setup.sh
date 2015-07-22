366d42d0f674150356257351e240a93857d39b59
Sly_tom_cat <slytomcat@mail.ru>
2015-07-22 20:38:03 +0300

    small changes

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ rm -rf /boot/*
 sed -i 's/\/boot\/efi/\/boot/' /etc/fstab
 mount /boot
 cp usr/bin/uefiboot-update /usr/bin/
-cp etc/systemd/uefiboot.cfg /etc/
+cp etc/uefiboot.cfg /etc/
 cp etc/systemd/system/uefiboot-update.* /etc/systemd/system/
 systemctl enable uefiboot-update.path
 systemctl start uefiboot-update.path
