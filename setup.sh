01585f323114f6fcf5e7506ccfb3ab1d9be8310d
Sly_tom_cat <slytomcat@mail.ru>
2015-07-22 10:44:54 +0300

    default values from /etc/fstab

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 mv /boot/*-generic /boot/EFI/
 umount /boot/EFI
 rm -rf /boot/*
-mount /dev/sda1 /boot
+sed -i 's/\/boot\/efi/\/boot/' /etc/fstab
+mount /boot
+cp usr/bin/uefiboot-update /usr/bin/
+cp etc/systemd/uefiboot.cfg /etc/
+cp etc/systemd/system/uefiboot-update.* /etc/systemd/system/
 systemctl enable uefiboot-update.path
 systemctl start uefiboot-update.path
-chmod /usr/bin/uefiboot-update a+x
+chmod a+x /usr/bin/uefiboot-update 
 apt-get purge grub*, shim
